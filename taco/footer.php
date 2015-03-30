6d5aaf67b8889690d8fc84a43a8c42e383d9ec71
prutaniii <paulrutaniii@MacBookPro.local>
2015-03-29 22:21:31 -0700

    Reverting back to last taco
    Original branch merged this evening has issues.
    This is to replace Paul-Search-Styling-1.

--- taco/footer.php
+++ taco/footer.php
@@ -31,7 +31,11 @@
 			</div>
 		</div>
 		<div class="footer-border">
+<<<<<<< HEAD
+			<!-- <img class="border-top" src="<?php bloginfo('template_directory'); ?>/images/searchborder.png" alt=""> -->
+=======
 			<img class="border-top" src="<?php bloginfo('template_directory'); ?>/images/searchborder.png" alt="">
+>>>>>>> origin/Paul-Search-Styling-1
 		</div>
 	</footer>
 
--- taco/page-home.php
+++ taco/page-home.php
@@ -33,6 +33,7 @@ Template Name: Home Page
 <head>
 	<title><?php if(is_home()) bloginfo('name'); else wp_title(''); ?></title>
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.min.css">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory') ?>/css/fonts.css">
 	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
@@ -42,7 +43,11 @@ Template Name: Home Page
 </head>
 <header>
 	<div class="head">
+
+		<!-- <div class="container">
+
 		<div class="container">
+>>>>>>> origin/Paul-Search-Styling-1
 			<div class="row">
 				<div class="col-xs-4 col-xs-offset-8">
 					<div class="login-btn-container">
@@ -56,17 +61,32 @@ Template Name: Home Page
 					</div>
 				</div>
 			</div>
+
+		</div> -->
+		<div class="row">
+			
+				<i class="fa fa-user fa-3x pull-right">
+					<span id="login" class="hidden-xs">
+					<?php 
+						if(is_user_logged_in()){
+							echo '<a href="'.get_permalink(71).'&logout=true">Logout</a>';
+						}else {
+							echo '<a href="'.get_permalink(68).'">Login</a>';
+						}
+						?>
+				 	</span>
+				</i>
 		</div>
 		<div class="row text-center">
 			<img src="<?php bloginfo('template_directory'); ?>/images/taco_snob.png" alt="">
 		</div>
 		<div class="row">	
 			<div class="search-container">
-				<div class="text-center">
+				<div class="col-xs-8 col-xs-offset-2">
 					<form action="<?php echo get_permalink(66); ?>" method="post" class="navbar-form">
 						<div class="form-group">						
-							<input name="location" class="form-control" type="text">
-							<button type="submit" class="btn btn-loud" value=""><span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+							<input name="location" class="form-control" type="text" placeholder="search">
+							<button type="submit" class="btn btn-loud" value="" ><span class="glyphicon glyphicon-search" aria-hidden="true"></span>
 							</button>
 						</div>
 					</form>
@@ -75,9 +95,13 @@ Template Name: Home Page
 		</div>
 	</div>
 </header>
-<div class="body">
+<div class="about">
 	<div class="container">
+
+		<div class="col-xs-8 col-xs-offset-2">
+
 		<div class="col-xs-8 col-xs-offset-2 about">
+
 			<img src="<?php bloginfo('template_directory'); ?>/images/aztec.png" alt="">
 			<h3>About</h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia id est debitis repellat omnis possimus expedita eligendi, voluptatum nobis, doloribus quasi consectetur itaque nihil illum quos a necessitatibus cumque cum.</p>
