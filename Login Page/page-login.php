d81b76f16f9bda5a6a2d5b6fe98c41f23ea2499a
prutaniii <paulrutaniii@MacBookPro.local>
2015-03-31 22:57:39 -0700

    taco theme nuke
    Deleting taco theme from this branch.

--- Login Page/page-login.php
+++ Login Page/page-login.php
@@ -3,21 +3,9 @@
 Template Name: Login Page
 */
 ?>
+
 <div class="container">
-	<div class="col-xs-6 col-xs-offset-3 login-container">
-		<form action="<?php echo get_permalink(71); ?>" method="post">
-			<div>
-				<span>Username:</span>
-				<input name="username" type="text">	
-			</div>
-			<div>
-				<span>Password</span>
-				<input name="password" type="password">	
-			</div>
-			<div>
-				<input type="submit" class="btn btn-primary pull-right" value="login">	
-			</div>
-		</form>
+	<div class="col-xs-6 col-xs-offset-2 login-container">
 		<div>
 			<p>Not a member? Register bitch!</p>
 			<form action="<?php echo get_permalink(71); ?>" method="post">
@@ -35,6 +23,20 @@ Template Name: Login Page
 				</div>
 				<div>
 					<input type="submit" class="btn btn-primary pull-right" value="register">	
+				</div>
+			</form>
+			<h3>Log In</h3>
+			<form action="<?php echo get_permalink(71); ?>" method="post">
+				<div>
+					<span>Username:</span>
+					<input name="username" type="text">	
+				</div>
+				<div>
+					<span>Password</span>
+					<input name="password" type="password">	
+				</div>
+				<div>
+					<input type="submit" class="btn btn-primary pull-right" value="login">	
 				</div>
 			</form>
 		</div>
